4cbcbceab08a02b0401cdab53e3a4e5cb78d3590
rj1995-dev <ram95.jana@gmail.com>
2019-10-15 11:40:40 +0530

    Create server & DecodePath

---
+++ playground/path.js
@@ -0,0 +1,4 @@
+const path = require("path");
+console.log(path.join("Node", "Project"));
+console.log(path.normalize("../../Node/../../Built in Modules fs"));
+console.log(path.resolve("../../Node/../Built in Modules fs"));
